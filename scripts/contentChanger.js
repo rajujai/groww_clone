ae75aaa4d1242a88211d7862c37f28873ec1ca9b
rajujai <rajujaijangra@gmail.com>
2022-02-23 14:59:52 +0530

    Initial setup

---
+++ scripts/contentChanger.js
@@ -0,0 +1,12 @@
+export default (optionsId, contentId)=>{
+    let list = document.getElementById(optionsId).children,
+    l = list.length;
+    let content = document.getElementById(contentId).children;
+    for(let item of list){
+        item.addEventListener("click", ()=>{
+            for(let i=0;i<l;i++)
+            content[i].classList.add("hide");
+            content[item.id].classList.remove("hide");
+        });
+    }
+}
---
+++ scripts/index.js
@@ -0,0 +1,2 @@
+import changeContent from "./contentChanger.js";
+changeContent("p2-options-list", "p2-options-content");
