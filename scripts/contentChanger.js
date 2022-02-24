15d56651f8638e2fa29b0d65042c0597ebf68114
rajujai <rajujaijangra@gmail.com>
2022-02-24 15:56:47 +0530

    prelogin section2 completed no animation

--- scripts/contentChanger.js
+++ scripts/contentChanger.js
@@ -4,9 +4,12 @@ export default (optionsId, contentId)=>{
     let content = document.getElementById(contentId).children;
     for(let item of list){
         item.addEventListener("click", ()=>{
-            for(let i=0;i<l;i++)
-            content[i].classList.add("hide");
+            for(let i=0;i<l;i++){
+                content[i].classList.add("hide");
+                list[i].classList.remove("bbtm");
+            }
             content[item.id].classList.remove("hide");
+            item.classList.add("bbtm");
         });
     }
 }
